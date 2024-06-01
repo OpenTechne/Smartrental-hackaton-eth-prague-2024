dfce670eb14cca04b69a17ecce3bbc81d549786a
Albina <albina.nikiforova.5670@gmail.com>
2024-06-01 17:29:08 +0200

    feat: page logic

--- packages/front-end/app/page.js
+++ packages/front-end/app/page.js
@@ -18,10 +18,14 @@ export default function Home() {
   };
 
   const [contract, setContract] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+  const [view, setView] = useState("UPLOAD");
 
   const generateContract = async (document) => {
     console.log("Generating contract from document:", document);
     // setContract(document);
+    setLoading(true);
+    setView("LOADING");
     try {
       const res = await fetch("/api/generate", {
         method: "POST",
@@ -38,31 +42,58 @@ export default function Home() {
       }
 
       const data = await res.json();
-      setResponse(data);
+      // setResponse(data);
+      if (data) {
+        setView("FORM");
+      }
     } catch (err) {
-      setError(err.message);
+      console.log(err.message);
     } finally {
       setLoading(false);
     }
   };
 
   // const contract = true;
-  if (contract) {
-    console.log(contract);
-  } else {
-    console.log("no contract");
-  }
 
-  return contract ? (
-    <DynamicForm
-      fields={RENTAL_FORM_FIELDS}
-      sections={RENTAL_FORM_SECTIONS}
-      onSubmit={handleSubmit}
-      columns={2}
-    />
-  ) : (
-    <FileUploader onUpload={generateContract} />
-    // <LoadingPage />
-    // <UserEnv />
-  );
+  // return contract ? (
+  //   <DynamicForm
+  //     fields={RENTAL_FORM_FIELDS}
+  //     sections={RENTAL_FORM_SECTIONS}
+  //     onSubmit={handleSubmit}
+  //     columns={2}
+  //   />
+  // ) : (
+  //   <FileUploader onUpload={generateContract} />
+  //   // <LoadingPage />
+  //   // <UserEnv />
+  // );
+
+  const renderView = () => {
+    console.log(view);
+    switch (view) {
+      case "UPLOAD":
+        return <FileUploader onUpload={generateContract} />;
+      case "LOADING":
+        return <LoadingPage />;
+      case "FORM":
+        return (
+          <DynamicForm
+            fields={RENTAL_FORM_FIELDS}
+            sections={RENTAL_FORM_SECTIONS}
+            onSubmit={handleSubmit}
+            columns={2}
+          />
+        );
+      case "NOTIFY":
+        return <NotifyPage />;
+      case "USER_ENV":
+        return <UserEnv />;
+      case "LOGIN":
+        return <div>Login Page</div>; // Add your login component or logic here
+      default:
+        return <div>Invalid view</div>;
+    }
+  };
+
+  return <div>{renderView()}</div>;
 }
